ab07c355971e8a5a02b392e96eb659adb456117f
Sharon <sharon-vaughan@bigpond.com>
2017-04-06 15:17:31 +1000

    Feat: Add render
    Body: Controller action can render for a redirect to a new controller and to different URL

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,18 @@ class DemoController < ApplicationController
 	layout false
 
   def index
+  	# render('index') default setting Generally used with if-then-else
   end
+
+  def hello
+  end
+
+  def other_hello
+  	# redirect_to(:action => 'hello') Generally useed to redirect to another Controller
+  end
+
+  def lynda
+  	redirect_to('http://lynda.com') 
+  end
+
 end
