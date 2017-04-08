50629d2d23063a636f44e8bad460acceb9047d28
Sharon <sharon-vaughan@bigpond.com>
2017-04-08 15:22:22 +1000

    Feat: Add models and named scopes
    Body: Three models added page, section and subject.
    Also five named scopes to subject.

---
+++ app/models/admin_user.rb
@@ -0,0 +1,8 @@
+class AdminUser < ApplicationRecord
+
+	# Use below if using legacy db with weird name
+	# self.table_name = "admin_users"
+
+
+
+end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
+
+	# place to put code that is common to all of our models
+	# ActiveRecord::Base is the core part of Rails where we're adding behaviours to our class
   self.abstract_class = true
 end
